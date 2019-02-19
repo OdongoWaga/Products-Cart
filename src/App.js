9c79529424da41ad8ba3fefc4bbd88b2d9a863d3
Waga Odongo <wagaodongo@gmail.com>
2019-02-19 19:55:44 +0300

    init
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import ProductsPage from './pages/Products';
+import ShopContext from  './context/shop-context';
+import CartPage from './pages/Cart';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ProductsPage} exact />
+          <Route path="/cart" component={CartPage} exact />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
